4ce4a985dedf15aecda48162ccccea602905896a
Hromor <aon-work@yandex.ru>
2022-10-24 21:31:21 +0300

    new file task_score

---
+++ task_score.js
@@ -0,0 +1,8 @@
+let intro = 30;
+let git = 335;
+let js = 100;
+let averageNumber = (intro + git + js) / 3;
+console.log(`Average score for homework is ${averageNumber}`);
+
+let fullName = "Oleg Apanasevich";
+console.log(`Average score for ${fullName}'s homework is ${averageNumber}🤪`);
