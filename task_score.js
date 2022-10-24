b3b6f7657a073b28e12ef08553258e69620d2ffc
Hromor <aon-work@yandex.ru>
2022-10-24 21:44:54 +0300

    added method slice

--- task_score.js
+++ task_score.js
@@ -5,4 +5,6 @@ let averageNumber = (intro + git + js) / 3;
 console.log(`Average score for homework is ${averageNumber}`);
 
 let fullName = "Oleg Apanasevich";
-console.log(`Average score for ${fullName}'s homework is ${averageNumber}🤪`);
+console.log(
+  `Average score for ${fullName.slice(0, 1)}'s homework is ${averageNumber}🤪`
+);
